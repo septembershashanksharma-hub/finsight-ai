b94162cd4b060536716a84dd2c25fb0f3b8410fa
Shashank Sharma <septembershashanksharma@gmail.com>
2026-05-21 11:30:53 +0530

    Week 2: Day 9 - designed and created 4-table relational database schema

--- src/data_queries.py
+++ src/data_queries.py
@@ -1,7 +1,9 @@
+import pandas as pd
+
 def load_clean_data(filepath):
-    import pandas as pd
     return pd.read_csv(filepath)
 
 
 def get_default_rate_by_grade(df):
-    
+    return df.groupby(df['grade'])['is_default'].mean() * 100
+
